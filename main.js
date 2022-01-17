6ed06e6e695109ca882322d6aa9355e79ff60032
iorir <mralkann@gmail.com>
2022-01-17 19:16:08 +0300

    code has been shortened

--- main.js
+++ main.js
@@ -2,6 +2,37 @@ let tdList = document.querySelector("#tdList");
 let input = document.querySelector("#input");
 let addButton = document.querySelector("#addButton");
 let toDoArray = JSON.parse(localStorage.getItem("tdos")) || [];
+const ifElseChecked = () => {
+  toDoArray.map((item, index) => {
+    if (item.isChecked === "false") {
+      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-dark" id="checkedItem${index}">
+      <div class="flex-fill" onClick="toggleItems(${index})" >
+        <i class="bi invisible bi-check-lg" id="checkedSVG${index}"></i>
+        <a
+          href="#"
+          id="checkedItemText${index}"
+          class="flex-fill ms-2 text-decoration-none text-dark"
+          
+          >${item.job}</a>
+      </div>
+      <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
+    </div>`;
+    } else {
+      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-white lightB" id="checkedItem${index}">
+      <div class="flex-fill" onClick="toggleItems(${index})" >
+        <i class="bi visible bi-check-lg" id="checkedSVG${index}"></i>
+        <a
+          href="#"
+          id="checkedItemText${index}"
+          class="flex-fill ms-2 text-decoration-none text-white"
+          
+          >${item.job}</a>
+      </div>
+      <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
+    </div>`;
+    }
+  });
+};
 
 const toggleItems = (e) => {
   let checkedItem = document.querySelector(`#checkedItem${e}`);
@@ -29,35 +60,7 @@ const addFunction = () => {
     toDoArray.push({ job: input.value, isChecked: "false" });
     localStorage.setItem("tdos", JSON.stringify(toDoArray));
     tdList.innerHTML = "";
-    toDoArray.map((item, index) => {
-      if (item.isChecked === "false") {
-        tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-dark" id="checkedItem${index}">
-        <div class="flex-fill" onClick="toggleItems(${index})" >
-          <i class="bi invisible bi-check-lg" id="checkedSVG${index}"></i>
-          <a
-            href="#"
-            id="checkedItemText${index}"
-            class="flex-fill ms-2 text-decoration-none text-dark"
-            
-            >${item.job}</a>
-        </div>
-        <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-      </div>`;
-      } else {
-        tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-white lightB" id="checkedItem${index}">
-        <div class="flex-fill" onClick="toggleItems(${index})" >
-          <i class="bi visible bi-check-lg" id="checkedSVG${index}"></i>
-          <a
-            href="#"
-            id="checkedItemText${index}"
-            class="flex-fill ms-2 text-decoration-none text-white"
-            
-            >${item.job}</a>
-        </div>
-        <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-      </div>`;
-      }
-    });
+    ifElseChecked();
   }
 };
 
@@ -66,72 +69,12 @@ addButton.addEventListener("click", () => {
 });
 
 if (toDoArray) {
-  toDoArray.map((item, index) => {
-    if (item.isChecked === "false") {
-      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-dark" id="checkedItem${index}">
-          <div class="flex-fill" onClick="toggleItems(${index})" >
-            <i class="bi invisible bi-check-lg" id="checkedSVG${index}"></i>
-            <a
-              href="#"
-              id="checkedItemText${index}"
-              class="flex-fill ms-2 text-decoration-none text-dark"
-              
-              >${item.job}</a>
-          </div>
-          <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-        </div>`;
-    } else {
-      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-white lightB" id="checkedItem${index}">
-          <div class="flex-fill" onClick="toggleItems(${index})" >
-            <i class="bi visible bi-check-lg" id="checkedSVG${index}"></i>
-            <a
-              href="#"
-              id="checkedItemText${index}"
-              class="flex-fill ms-2 text-decoration-none text-white"
-              
-              
-              >${item.job}</a>
-          </div>
-          <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-        </div>`;
-    }
-  });
+  ifElseChecked();
 }
 
 const deleteItem = (index) => {
- 
-    tdList.innerHTML="";
-    toDoArray.splice(index, 1);
-    localStorage.setItem("tdos", JSON.stringify(toDoArray));
-
-  toDoArray.map((item, index) => {
-    if (item.isChecked === "false") {
-      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-dark" id="checkedItem${index}">
-              <div class="flex-fill" onClick="toggleItems(${index})" >
-                <i class="bi invisible bi-check-lg" id="checkedSVG${index}"></i>
-                <a
-                  href="#"
-                  id="checkedItemText${index}"
-                  class="flex-fill ms-2 text-decoration-none text-dark"
-                  
-                  >${item.job}</a>
-              </div>
-              <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-            </div>`;
-    } else {
-      tdList.innerHTML += `<div class="d-flex list-group-item  text-decoration justify-content-between text-white lightB" id="checkedItem${index}">
-              <div class="flex-fill" onClick="toggleItems(${index})" >
-                <i class="bi visible bi-check-lg" id="checkedSVG${index}"></i>
-                <a
-                  href="#"
-                  id="checkedItemText${index}"
-                  class="flex-fill ms-2 text-decoration-none text-white"
-                  
-                  
-                  >${item.job}</a>
-              </div>
-              <i class="bi bi-x-lg" onClick="deleteItem(${index})"></i>
-            </div>`;
-    }
-  });
+  tdList.innerHTML = "";
+  toDoArray.splice(index, 1);
+  localStorage.setItem("tdos", JSON.stringify(toDoArray));
+  ifElseChecked();
 };
